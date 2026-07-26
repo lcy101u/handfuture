2ba06be0f70dca89dae2a861d5c271f15458de1a
lcy101u <phanton0117@gmail.com>
2026-07-27 00:32:55 +0800

    build: regenerate AdSense-ready distribution

---
+++ dist/assets/PrivacyPolicyPage-zUh4dILC.js
@@ -0,0 +1,1 @@
+import{D as e,O as t}from"./index-B2OjdI3X.js";import{n}from"./policies-BLDsPP2s.js";import{EditorialArticle as r}from"./GuidePage-Dr66KhRF.js";var i=e();function a(){let e=t(e=>e.currentLanguage);return(0,i.jsx)(r,{content:n[e],locale:e,relatedPaths:[`/how-it-works`,`/guides/hand-photo-guide`],eyebrow:e===`zh`?`隱私與資料處理`:`Privacy and data handling`})}export{a as default};
